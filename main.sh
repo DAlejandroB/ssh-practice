e9361950b3345ab136ab1c51b9a30203155e9240
DAlejandroB <diegoaenciso76@gmail.com>
2021-07-15 22:36:15 -0500

    All bash commands moved into a single file, including sshpass package instalation

--- main.sh
+++
@@ -1,1 +0,0 @@
-sudo sshpass -p '123' ssh 192.168.1.74 < tasks.sh
--- tasks.sh
+++ tasks.sh
@@ -1,3 +1,10 @@
+#Check if sshpass package is installed, sshpass makes it easier to work with password ssh authentication
+if ["" = $(dpkg-query -W --showformat='${Status}\n' sshpass|grep "install ok installed")]; then
+	echo "No sshpass installed, setting up sshpass"
+	sudo apt-get --yes install sshpass
+fi
+
+sshpass -p "123" ssh 192.168.1.74 << 'ENDSSH'
 #Batch program made to check if the current directory is root
 VAR1="/root"
 VAR2="$PWD"
@@ -6,4 +13,4 @@ if [ "$VAR1" = "$VAR2" ]; then
 else
 	echo "Failure, the directory is no root :( "
 fi
-
+ENDSSH
